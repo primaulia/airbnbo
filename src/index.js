6473637c8018aa961c6d938e4a223e87cd2bdbca
Prima Aulia Gusta <primAulia@gmail.com>
2018-02-12 13:47:16 +0800

    react ready to lister for new subs data

--- src/index.js
+++ src/index.js
@@ -9,8 +9,33 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// this is the link to connect to ur graphql server
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+// This packages is for ws connection
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+// this is the link to connect to ur graphql server (HTTP only!)
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' }) // TODO: update this to process.env
+
+// create one more link for WS
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true
+  }
+})
+
+const splitFn = ({query}) => {
+  const { kind, operation } = getMainDefinition(query)
+  return kind === 'OperationDefinition' && operation === 'subscription'
+}
+
+// split the method according to the request ( type of operation )
+const link = split(
+  splitFn,
+  wsLink,
+  httpLink
+)
 
 // this is the object that helps to run the graphql query
 // or mutation for you
